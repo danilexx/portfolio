36ee9c0afe759ab117bfa57bfe755ae0d87f9124
Danilo de Jesus <danilo.de.jesus.dev@gmail.com>
2020-03-19 20:18:15 -0300

    feat: added title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,17 +7,22 @@ import Projects from "src/sections/projects";
 import Technologies from "src/sections/technologies";
 import Contact from "src/sections/contact";
 import Footer from "src/components/Footer";
+import { useTitle } from "react-use";
 
-const Home: React.FC = () => (
-  <Layout>
-    <ApresentationSection />
-    <Nav />
-    <About />
-    <Projects />
-    <Technologies />
-    <Contact />
-    <Footer />
-  </Layout>
-);
+const Home: React.FC = () => {
+  useTitle("Danilo de Jesus");
+
+  return (
+    <Layout>
+      <ApresentationSection />
+      <Nav />
+      <About />
+      <Projects />
+      <Technologies />
+      <Contact />
+      <Footer />
+    </Layout>
+  );
+};
 
 export default Home;
